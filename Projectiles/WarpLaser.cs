d8312423cfe8f8137b0055051c8c4b4621c20a41
Frozelar <frostyfish88@gmail.com>
2018-11-23 11:57:15 -0500

    More work on Warp Laser
    - Updated warp laser graphics
    - Continued working on warp laser code
      - TODO: Rewrite everything?
    - Made placeholder Extraterrestrian graphics
      - TODO: Make actual graphics

--- Projectiles/WarpLaser.cs
+++ Projectiles/WarpLaser.cs
@@ -14,8 +14,17 @@ namespace ExampleMod.Projectiles
 	// Using custom drawing, dust effects, and custom collision checks for tiles
 	public class WarpLaser : ModProjectile
 	{
+        // The maximum length for the laser
+        private const int MaxHeight = 200;
+
+        // Flag: has the laser reached its max length?
+        private const bool ReachedMaxHeight = false;
+
+        // Flag: has the laser reached its min length?
+        private const bool ReachedMinHeight = false;
+
 		//The distance charge particle from the player center
-		private const float MoveDistance = 60f;
+		//private const float MoveDistance = 60f;
 
         // The actual distance is stored in the ai0 field
         // By making a property to handle this it makes our life easier, and the accessibility more readable
@@ -25,10 +34,16 @@ namespace ExampleMod.Projectiles
             set { projectile.ai[0] = value; }
         }
 
+        public int Height
+        {
+            get { return (int)projectile.ai[1]; }
+            set { projectile.ai[1] = value; }
+        }
+
         public override void SetDefaults()
 		{
-			projectile.width = 10;
-			projectile.height = 10;
+			projectile.width = 18;
+			projectile.height = 2;
 			projectile.friendly = false;
 			projectile.penetrate = -1;
 			projectile.tileCollide = false;
@@ -38,6 +53,21 @@ namespace ExampleMod.Projectiles
 
         public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
         {
+            Vector2 origin = Main.npc[projectile.owner].Center;
+            float r = projectile.velocity.ToRotation() + -1.57f;
+
+            //for (float i = (int)MoveDistance; i <= Distance; i += 10)
+            //{
+            Color c = Color.White;
+            // origin = Main.npc[projectile.owner].Center + projectile.velocity;//i * projectile.velocity;
+            origin = Main.npc[projectile.owner].TopRight + projectile.velocity;
+            spriteBatch.Draw(Main.projectileTexture[projectile.type], origin - Main.screenPosition,
+                new Rectangle(0, 0, 18, (Height > 200 ? 200 : Height)), c, r,
+                new Vector2(28 * .5f, 26 * .5f), 1f, 0, 0);
+            //}
+
+
+            /*
             for (int k = 0; k < projectile.oldPos.Length; k++)
             {
                 if (projectile.oldPos[k] == Vector2.Zero)
@@ -48,42 +78,70 @@ namespace ExampleMod.Projectiles
                 Color color = Lighting.GetColor((int)(projectile.oldPos[k].X / 16f), (int)(projectile.oldPos[k].Y / 16f));
                 spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, 0f, projectile.Size / 2f, 1f, SpriteEffects.None, 0f);
             }
+            */
         }
 
+        // TODO: THIS IS PROBABLY WRONG
         public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
         {
-            for (int k = 0; k < projectile.oldPos.Length; k++)
-            {
-                if (projectile.oldPos[k] == Vector2.Zero)
-                {
-                    return null;
-                }
-                projHitbox.X = (int)projectile.oldPos[k].X;
-                projHitbox.Y = (int)projectile.oldPos[k].Y;
-                if (projHitbox.Intersects(targetHitbox))
-                {
-                    return true;
-                }
-            }
-            return null;
+            Player player = Main.player[projectile.owner];
+            Vector2 unit = projectile.velocity;
+            float point = 0f;
+            // Run an AABB versus Line check to look for collisions, look up AABB collision first to see how it works
+            // It will look for collisions on the given line using AABB
+            return Collision.CheckAABBvLineCollision(player.Hitbox.TopLeft(), player.Hitbox.Size(), targetHitbox.TopRight(),
+                targetHitbox.TopRight() + unit * Distance, 18, ref point);
         }
-
-        // Set custom immunity time on hitting an NPC
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
-		{
-			target.immune[projectile.owner] = 5;
-		}
 
 		// The AI of the projectile
 		public override void AI()
 		{
 			Vector2 mousePos = Main.MouseWorld;
 			Player player = Main.player[projectile.owner];
+            NPC npc = Main.npc[projectile.owner];
             
-			//Add lights
-			DelegateMethods.v3_1 = new Vector3(0.8f, 0.8f, 1f);
+            Vector2 diff = player.Center - npc.TopRight;
+            diff.Normalize();
+            projectile.velocity = diff;
+            projectile.direction = player.position.X > npc.position.X ? 1 : -1;
+            projectile.netUpdate = true;
+            projectile.timeLeft = 2;
+            
+            if (Height < MaxHeight)
+            {
+                if (!ReachedMaxHeight)
+                {
+                    Height += (int)projectile.velocity.Length();
+                }
+                else if (ReachedMaxHeight && !ReachedMinHeight)
+                {
+                    Height -= (int)projectile.velocity.Length();
+                    projectile.position = npc.TopRight + projectile.velocity;
+                }
+                else
+                {
+                    projectile.position = npc.TopRight + projectile.velocity;// * MoveDistance;
+                }
+            }
+            projectile.height = Height;
+            Distance = projectile.Hitbox.Size().Length();
+
+
+            /*
+            int dir = projectile.direction;
+            player.ChangeDir(dir);
+            player.heldProj = projectile.whoAmI;
+            player.itemTime = 2;
+            player.itemAnimation = 2;
+            player.itemRotation = (float)Math.Atan2(projectile.velocity.Y * dir, projectile.velocity.X * dir);
+            */
+
+            /*
+            //Add lights
+            DelegateMethods.v3_1 = new Vector3(0.8f, 0.8f, 1f);
 			Utils.PlotTileLine(projectile.Center, projectile.Center + projectile.velocity * (Distance - MoveDistance), 26,
 				DelegateMethods.CastLight);
+                */
 		}
 
 		public override bool ShouldUpdatePosition()
